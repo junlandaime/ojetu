ee1be9f49a6ebfac1bd79babb6f16f60d5bcf27b
Jundi Abdullah <jdi.jundi99@gmail.com>
2025-11-17 22:16:35 +0700

    build perubahan di main jsx dan configjs api api

--- frontend/dist/config.js
+++ frontend/dist/config.js
@@ -2,7 +2,7 @@
 // Ubah nilai di bawah ini tanpa rebuild bila perlu.
 window.__APP_CONFIG__ = {
   // Contoh: "https://api.domainanda.com/api"
-  VITE_API_URL: "https://try.fitalenta.co.id/api",
+  VITE_API_URL: "https://try.fitalenta.co.id",
   // Contoh: "https://cdn.domainanda.com/uploads"
   VITE_FILE_BASE_URL: "https://registrasi.fitalenta.co.id",
 };
